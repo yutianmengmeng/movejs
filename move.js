e1f0aa100f0677b4a2eedb95983f5452d78a44c9
roundDB <1643453298@qq.com>
2017-02-23 15:56:50 +0800

    fake-flash

---
+++ move.js
@@ -0,0 +1,40 @@
+function getStyle(obj,attr){
+			if(obj.currentStyle){
+				return obj.currentStyle.attr;
+			}else{
+				return getComputedStyle(obj,false)[attr];
+			}
+		}
+function startMove(obj,attr,iTarget){
+			clearInterval(obj.timer);
+			obj.timer=setInterval(function(){
+				var cur=0;
+				if(attr=='opacity'){
+					cur=Math.round(parseFloat(getStyle(obj,attr))*100);
+				}else{
+					cur=parseInt(getStyle(obj,attr));
+				}
+				
+				var speed=(iTarget-cur)/7;
+				speed=speed>0?Math.ceil(speed):Math.floor(speed);
+
+				if(Math.abs(iTarget-cur)<7){
+					clearInterval(obj.timer);
+					if(attr=="opacity"){
+						obj.style.filter="alpha(opacity:"+iTarget+")";
+						obj.style.opacity=iTarget/100
+					}else{
+						obj.style[attr]=iTarget+"px";
+					}
+				}else{
+					if(attr=='opacity'){
+						obj.style.filter="alpha(opacity:"+(cur+speed)+")";
+						obj.style.opacity=(cur+speed)/100
+					}else{
+						obj.style[attr]=cur+speed+"px";
+					}
+					
+				}
+			},30)
+
+		}
